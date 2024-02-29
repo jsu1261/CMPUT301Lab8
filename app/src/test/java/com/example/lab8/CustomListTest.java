f83d0cbdf3f82d1a2caed546e0f5c7c86a103c91
jsu1261 <jennysu12@outlook.com>
2024-02-29 16:11:35 -0700

    create deleteCity test

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -2,6 +2,7 @@ package com.example.lab8;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
@@ -38,6 +39,10 @@ public class CustomListTest {
         assertEquals(list.getCount(),listSize + 1);
     }
 
+    /**
+     * add a city to the list
+     * check if it's true that the city is in the list
+     */
     @Test
     public void hasCityTest() {
         list = MockCityList();
@@ -45,5 +50,22 @@ public class CustomListTest {
         list.addCity(city);
         assertTrue(list.hasCity(city));
     }
+
+    /**
+     * add a city to the list
+     * check that the city is in the list
+     * delete the city
+     * check that the city is no longer in the list
+     */
+    @Test
+    public void deleteCityTest() {
+        list = MockCityList();
+        City city = new City("Vancouver", "BC");
+        list.addCity(city);
+        assertTrue(list.hasCity(city));
+        list.deleteCity(city);
+        assertFalse(list.hasCity(city));
+    }
+
 
 }
